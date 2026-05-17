b6570144b9cdf43d6dcc0b28cba9f5f584e1c3fb
Hasaan Azeem <hasaanazeem07@gmail.com>
2026-05-17 23:56:31 +0500

    New

--- lib/pdf/utils.ts
+++ lib/pdf/utils.ts
@@ -7,14 +7,20 @@ export function downloadBlob(
   mimeType = "application/pdf"
 ) {
   const blob =
-    data instanceof Blob ? data : new Blob([data], { type: mimeType });
+    data instanceof Blob
+      ? data
+      : new Blob([data.buffer as ArrayBuffer], { type: mimeType });
+
   const url = URL.createObjectURL(blob);
+
   const a = document.createElement("a");
   a.href = url;
   a.download = filename;
+
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
+
   setTimeout(() => URL.revokeObjectURL(url), 1000);
 }
 
